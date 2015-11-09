c5e87c104841e9e83a00d6f42daec212a63e6eae
Claus Höfele <claus@claushoefele.com>
2015-11-09 20:31:22 +0100

    Add water layer

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -2,3 +2,5 @@
 
 rm -f qgis/layers/1690/streets.*
 ogr2ogr -f "ESRI Shapefile" qgis/layers/1690/streets.shp maps/1690/streets.geojson
+rm -f qgis/layers/1690/water.*
+ogr2ogr -f "ESRI Shapefile" qgis/layers/1690/water.shp maps/1690/water.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -2,3 +2,5 @@
 
 rm -f maps/1690/streets.geojson
 ogr2ogr -f "GeoJSON" maps/1690/streets.geojson qgis/layers/1690/streets.shp
+rm -f maps/1690/water.geojson
+ogr2ogr -f "GeoJSON" maps/1690/water.geojson qgis/layers/1690/water.shp
