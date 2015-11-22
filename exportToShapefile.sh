c85e8f40b4e37a9a19280dc2dd32ecad0c92b22c
Claus Höfele <claus@claushoefele.com>
2015-11-22 20:07:10 +0100

    Move generated shapefiles to new folder

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm -f qgis/layers/1690/streets.*
-mkdir -p qgis/layers/1690
-ogr2ogr -f "ESRI Shapefile" qgis/layers/1690/streets.shp maps/1690/streets.geojson
-rm -f qgis/layers/1690/water.*
-ogr2ogr -f "ESRI Shapefile" qgis/layers/1690/water.shp maps/1690/water.geojson
+rm -Rf qgis/layers/Berlin/1690
+mkdir -p qgis/layers/Berlin/1690
+ogr2ogr -f "ESRI Shapefile" qgis/layers/Berlin/1690/streets.shp maps/1690/streets.geojson
+ogr2ogr -f "ESRI Shapefile" qgis/layers/Berlin/1690/water.shp maps/1690/water.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f maps/1690/streets.geojson
-ogr2ogr -f "GeoJSON" maps/1690/streets.geojson qgis/layers/1690/streets.shp
-rm -f maps/1690/water.geojson
-ogr2ogr -f "GeoJSON" maps/1690/water.geojson qgis/layers/1690/water.shp
+rm -Rf maps/1690
+mkdir -p maps/1690
+ogr2ogr -f "GeoJSON" maps/1690/streets.geojson qgis/layers/Berlin/1690/streets.shp
+ogr2ogr -f "GeoJSON" maps/1690/water.geojson qgis/layers/Berlin/1690/water.shp
