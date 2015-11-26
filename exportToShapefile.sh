fd3a33a0f7d0427b3d8ef532b126cdc4ee5cd622
Claus Höfele <claus@claushoefele.com>
2015-11-26 20:19:22 +0100

    Rename layers to be comaptible with OSM

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -Rf qgis/layers/Berlin/1690
 mkdir -p qgis/layers/Berlin/1690
-ogr2ogr -f "ESRI Shapefile" qgis/layers/Berlin/1690/streets.shp maps/1690/streets.geojson
-ogr2ogr -f "ESRI Shapefile" qgis/layers/Berlin/1690/water.shp maps/1690/water.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -Rf maps/1690
+rm -Rf maps/1690 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1690
-ogr2ogr -f "GeoJSON" maps/1690/streets.geojson qgis/layers/Berlin/1690/streets.shp
-ogr2ogr -f "GeoJSON" maps/1690/water.geojson qgis/layers/Berlin/1690/water.shp
+ogr2ogr -f "GeoJSON" maps/1690/roads.geojson qgis/layers/Berlin/1690/roads.shp
+ogr2ogr -f "GeoJSON" maps/1690/natural.geojson qgis/layers/Berlin/1690/natural.shp
