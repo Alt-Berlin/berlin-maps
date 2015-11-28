76ec74c015a6a1397eb067bf7c2dc0c99e844949
Claus Höfele <claus@claushoefele.com>
2015-11-28 20:21:27 +0100

    Add water layer 1650

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mkdir -p qgis/layers/Berlin/1690
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1650/natural.shp maps/1650/natural.geojson
+
+mkdir -p qgis/layers/Berlin/1690
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads.geojson
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines.geojson
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -Rf maps/1650 # -overwrite not supported by GeoJSON Driver 
+mkdir -p maps/1650
+ogr2ogr -f "GeoJSON" maps/1650/natural.geojson qgis/layers/Berlin/1650/natural.shp
+
 rm -Rf maps/1690 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1690
 ogr2ogr -f "GeoJSON" maps/1690/roads.geojson qgis/layers/Berlin/1690/roads.shp
