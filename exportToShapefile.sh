16b20512736a04268510563750ac4257beedd86c
Claus Höfele <claus@claushoefele.com>
2015-11-28 19:55:44 +0100

    Add city wall layer

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -2,4 +2,5 @@
 
 mkdir -p qgis/layers/Berlin/1690
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines.geojson
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -3,4 +3,5 @@
 rm -Rf maps/1690 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1690
 ogr2ogr -f "GeoJSON" maps/1690/roads.geojson qgis/layers/Berlin/1690/roads.shp
+ogr2ogr -f "GeoJSON" maps/1690/lines.geojson qgis/layers/Berlin/1690/lines.shp
 ogr2ogr -f "GeoJSON" maps/1690/natural.geojson qgis/layers/Berlin/1690/natural.shp
