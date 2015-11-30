a279eed54f998f96aaf375d6830d2e0fa0663e5e
Claus Höfele <claus.hoefele@here.com>
2015-11-30 17:41:08 +0100

    Fix typo in script

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p qgis/layers/Berlin/1690
+mkdir -p qgis/layers/Berlin/1650
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1650/natural.shp maps/1650/natural.geojson
 
 mkdir -p qgis/layers/Berlin/1690
