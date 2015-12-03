92517343579548fc7ee02726fe99d9c3351b9c66
Claus Höfele <claus@claushoefele.com>
2015-12-03 22:26:36 +0100

    Give geojson file unique names

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p qgis/layers/Berlin/1650
-ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1650/natural.shp maps/1650/natural.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1650/natural.shp maps/1650/natural_1650.geojson
 
 mkdir -p qgis/layers/Berlin/1690
-ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads.geojson
-ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines.geojson
-ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads_1690.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines_1690.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural_1690.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -2,10 +2,10 @@
 
 rm -Rf maps/1650 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1650
-ogr2ogr -f "GeoJSON" maps/1650/natural.geojson qgis/layers/Berlin/1650/natural.shp
+ogr2ogr -f "GeoJSON" maps/1650/natural_1650.geojson qgis/layers/Berlin/1650/natural.shp
 
 rm -Rf maps/1690 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1690
-ogr2ogr -f "GeoJSON" maps/1690/roads.geojson qgis/layers/Berlin/1690/roads.shp
-ogr2ogr -f "GeoJSON" maps/1690/lines.geojson qgis/layers/Berlin/1690/lines.shp
-ogr2ogr -f "GeoJSON" maps/1690/natural.geojson qgis/layers/Berlin/1690/natural.shp
+ogr2ogr -f "GeoJSON" maps/1690/roads_1690.geojson qgis/layers/Berlin/1690/roads.shp
+ogr2ogr -f "GeoJSON" maps/1690/lines_1690.geojson qgis/layers/Berlin/1690/lines.shp
+ogr2ogr -f "GeoJSON" maps/1690/natural_1690.geojson qgis/layers/Berlin/1690/natural.shp
