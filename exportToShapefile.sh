d88663908c102f7252ca393245711c8e21f769db
Claus Höfele <claus@claushoefele.com>
2015-12-07 20:44:48 +0100

    Add boundary polygon

--- exportToShapefile.sh
+++ exportToShapefile.sh
@@ -5,5 +5,6 @@ ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1650/natural.shp maps/
 
 mkdir -p qgis/layers/Berlin/1690
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/roads.shp maps/1690/roads_1690.geojson
-ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines_1690.geojson
 ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/natural.shp maps/1690/natural_1690.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/lines.shp maps/1690/lines_1690.geojson
+ogr2ogr -f "ESRI Shapefile" -overwrite qgis/layers/Berlin/1690/polygons.shp maps/1690/polygons_1690.geojson
--- importFromShapefile.sh
+++ importFromShapefile.sh
@@ -7,5 +7,6 @@ ogr2ogr -f "GeoJSON" maps/1650/natural_1650.geojson qgis/layers/Berlin/1650/natu
 rm -Rf maps/1690 # -overwrite not supported by GeoJSON Driver 
 mkdir -p maps/1690
 ogr2ogr -f "GeoJSON" maps/1690/roads_1690.geojson qgis/layers/Berlin/1690/roads.shp
-ogr2ogr -f "GeoJSON" maps/1690/lines_1690.geojson qgis/layers/Berlin/1690/lines.shp
 ogr2ogr -f "GeoJSON" maps/1690/natural_1690.geojson qgis/layers/Berlin/1690/natural.shp
+ogr2ogr -f "GeoJSON" maps/1690/lines_1690.geojson qgis/layers/Berlin/1690/lines.shp
+ogr2ogr -f "GeoJSON" maps/1690/polygons_1690.geojson qgis/layers/Berlin/1690/polygons.shp
